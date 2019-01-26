7d546584ab0d13d94b9df0f5cd62e1599ef80c85
Justin Rubek <rubejus@bvu.edu>
2019-01-26 16:03:53 -0600

    Added better support for multi-monitor

--- polybar/.config/polybar/launch_single.sh
+++ polybar/.config/polybar/launch_single.sh
@@ -7,14 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-if type "xrandr"; then
-   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar top &
-    MONITOR=$m polybar bottom &
-  done
-else
-  polybar top &
-  polybar bottom &
+polybar top &
+polybar bottom &
 
-fi
 echo "Bars launched"
